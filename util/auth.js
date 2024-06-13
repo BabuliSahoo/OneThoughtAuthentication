d46ad27365cb96e329d486c200d91fd0d369add1
Babuli Sahoo <babuli.sahoo4@gmail.com>
2024-06-13 21:44:25 +0530

    key updated

--- util/auth.js
+++ util/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'AIzaSyBU1yaeYhSbBco2kAZsEpuqdO-yPudJYTs';
+const API_KEY = 'key';
 
 async function authenticate(mode, email, password) {
   const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
